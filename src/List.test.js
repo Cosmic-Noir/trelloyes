bc785b3134f4b675b22d592cb2d57ad88b3b70e9
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 16:20:23 -0600

    Added snapshot tests for Card.test.js and List.test.js - all tests passing

--- src/List.test.js
+++ src/List.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import List from "./List";
+import renderer from "react-test-renderer";
+const cards = [];
 
 describe("List component", () => {
   it("renders without crashing", () => {
-    const cards = [];
     const div = document.createElement("div");
     ReactDOM.render(<List cards={cards} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders the UI as expected", () => {
+    const tree = renderer.create(<List cards={cards} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
