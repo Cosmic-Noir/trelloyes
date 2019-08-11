ae00231468fe0daebcfc13bccf5d3c320051c7ed
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 15:58:31 -0600

    Add smokescreen tests for List and Card components, did not feed props yet

--- src/List.test.js
+++ src/List.test.js
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import List from "./List";
+
+describe("List component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
